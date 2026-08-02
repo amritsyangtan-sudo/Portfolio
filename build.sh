782ee99aead1870288910770d7a890af048a26dd
AmritS <amritsyangtan1@gmail.com>
2026-08-02 13:22:55 +0545

    build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c 10.0 --insta
 export PATH="$PWD/dotnet:$PATH"
 
 dotnet publish Portfolio/Portfolio.csproj -c Release -o output
+
+echo "===== OUTPUT ====="
+find output -maxdepth 3
